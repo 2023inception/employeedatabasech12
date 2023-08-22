9242a7f0323f0947bef3efe7e1fd0d87bba846e1
Mr. E <mr.e@Katrinas-iMac.local>
2023-08-22 18:33:45 -0400

    updated index.js

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+USE employee_tracker;
+
+INSERT INTO department(name) VALUES('legal'),('accountant'),('software');
--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const inquirer=require('mysql12');
+const inquirer=require('inquirer');
+const mysql=require('mysql2');
 const fs=require('fs');
 
 const connection = mysql.createConnection({
-host:
-user:
-password:
-database:
+host:"localhost",
+user:"root",
+password:"Football23!",
+database:"employee_tracker",
 });
 
-const
 const questions =[
     {
         type:'list',
         message: 'options',
+        name:'selection',
         choices:['view all departments','view all roles','view all employees','add a department','add a role','add an employee','update an employee role'],
     }
 ]
+inquirer.prompt(questions)
+.then((data)=> {
+if(data.selection==='view all departments'){
+ viewAlldepartments()
+}
+})
+function viewAlldepartments(){
+    connection.query("SELECT * FROM department",(error, rows)=>{
+        if (error) throw error
+        console.table(rows)
+    })
+}
